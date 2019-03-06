9eabe06ba478f19425068b8cc976f8d0570002fa
Geetika Jain <geetikajain@Geetikas-MacBook-Air.local>
2019-03-06 17:58:43 -0600

    adding more features

--- features.py
+++ features.py
@@ -1,3 +1,20 @@
+def removeSpecialCharacter(word):
+	cleanString = "";
+	for ch in word:
+		if ch.isalnum() or ch == " ":
+			cleanString = cleanString + str(ch)
+	return cleanString
+
+def removeApostrophS(word):
+	word = word.replace("'s", "")
+	return word
+
+def getDocumentContent(document):
+	path = "raw/"
+	filename = path + str(document) + ".txt"
+	file = open(filename,"r")
+	return file.read();
+
 def isStartOfSentence(offset, document):
 	if(offset == 0):
 		return True;
@@ -12,14 +29,10 @@ def isStartOfSentence(offset, document):
 			elif text[offset] == ".":
 				return True;
 
-def getDocumentContent(document):
-	path = "raw/"
-	filename = path + str(document) + ".txt"
-	file = open(filename,"r")
-	return file.read();
 
 def isContainPrefix(word):
 	listOfPrefixes = ["President", "DJ", "Captain", "Adm", "Atty", "Brother", "Capt", "Chief", "Cmdr", "Col", "Dean", "Dr", "Elder", "Father", "Gen", "Gov", "Hon", "Lt Col", "Maj", "MSgt", "Mr", "Mrs", "Ms", "Prince", "Prof", "Rabbi", "Rev", "Sister", "Sir", "Queen"]
+	word = removeSpecialCharacter(word)
 	firstWord = word.partition(' ')[0]
 	if firstWord in listOfPrefixes:
 		return True
@@ -28,6 +41,7 @@ def isContainPrefix(word):
 
 def isContainSuffix(word):
 	listOfSuffixes = ["II", "III", "IV", "CPA", "DDS", "Esq", "JD", "Jr", "LLD", "MD", "PhD", "Ret", "RN", "Sr", "DO"]
+	word = removeSpecialCharacter(word)
 	lastWord = word.partition(' ')[-1]
 	if lastWord in listOfSuffixes:
 		return True
@@ -39,17 +53,17 @@ def getPreviousWord(offset, text):
 	previousWord = ""
 	if text[previousWordOffset] == " ":
 		previousWordOffset = previousWordOffset - 1;
-		while text[previousWordOffset].isalnum():
+		while text[previousWordOffset] != " " and text[previousWordOffset] != ".":
 			previousWord = text[previousWordOffset] + previousWord
 			previousWordOffset = previousWordOffset - 1;
 	else:
 		previousWord = ""
-	return previousWord
+	return previousWord, previousWordOffset + 1
 
 def getNextWord(offset, text):
 	nextWordOffset = offset
 	nextWord = ""
-	while text[nextWordOffset].isalnum():
+	while text[nextWordOffset] != "." and text[nextWordOffset] != " ":
 		nextWordOffset = nextWordOffset + 1;
 	if text[nextWordOffset] == " ":
 		nextWordOffset = nextWordOffset + 1;
@@ -58,12 +72,19 @@ def getNextWord(offset, text):
 			nextWordOffset = nextWordOffset + 1
 	else:
 		nextWord = ""
+	# while text[nextWordOffset] == " " or text[nextWordOffset] == ".":
+	# 	nextWordOffset = nextWordOffset + 1
+	
+	return nextWord, nextWordOffset - len(nextWord)
 
 def isPartial(offset, document):
 	text = getDocumentContent(document)
 	
-	previousWord = getPreviousWord(offset, text)
-	nextWord = getNextWord(offset, text)
+	previousWord, previousOffset = getPreviousWord(offset, text)
+	nextWord, nextOffset = getNextWord(offset, text)
+
+	previousWord = removeSpecialCharacter(previousWord)
+	nextWord = removeSpecialCharacter(nextWord)
 		
 	return allWordsCapitalized(previousWord) or allWordsCapitalized(nextWord)
 
@@ -85,29 +106,122 @@ def hasFullNameOccurence(offset, document, word):
 	if(len(splits) > 1):
 		return False;
 	text = getDocumentContent(document)
+	word = removeApostrophS(word)
+	word = removeSpecialCharacter(word)
 	occurences = [i for i in range(len(text)) if text.startswith(word, i)]
 	for j in range(len(occurences)):
 		newOffset = occurences[j]
 		if newOffset == offset:
 			continue
 		else:
-			previousWord = getPreviousWord(newOffset, text)
-			nextWord = getNextWord(newOffset, text)
+			previousWord, previousOffset = getPreviousWord(newOffset, text)
+			nextWord, nextOffset = getNextWord(newOffset, text)
 			if allWordsCapitalized(previousWord) or allWordsCapitalized(nextWord):
 				return True
 	return False
 
+def allWordsCapitalized(candidateWord):
+	if len(candidateWord) <= 0:
+		return False
+	words = candidateWord.split()
+	for word in words:
+		if len(word) > 0 and (not word[0].isupper()):
+			return False
+	return True
+
+def isLocation(offset, document):
+	wordThreshold = 3
+	text = getDocumentContent(document);
+	locationDict = ["in", "on", "at", "near"]
+	for i in range(wordThreshold):
+		word, offset = getPreviousWord(offset, text)
+		word = word.lower()
+		if word != "":
+			if word in locationDict:
+				return True
+		else:
+			break;
+	return False
+
+def isPrecededByWords(offset, document):
+	text = getDocumentContent(document);
+	array = ["model", "actor", "actress", "singer", "musician", "star", "stars", "producer", "judges", "soap", "comedian ", "writer", "producer", "pianist", "guitarist", "drummer", "rapper", "activist", "cowriter", "featuring", "introducing", "starring"]
+	wordThreshold = 3
+	for i in range(wordThreshold):
+		word, offset = getPreviousWord(offset, text)
+		word = word.lower()
+		if word != "":
+			word = removeSpecialCharacter(word)
+			for ele in array:
+				ele = ele.lower()
+				if ele.startswith(word):
+					return True
+		else:
+			break;
+	return False
+
+def isSucceededByWords(offset, document):
+	text = getDocumentContent(document);
+	array = ["model", "actor", "actress", "singer", "musician", "star", "stars", "producer", "judges", "soap", "comedian ", "writer", "producer", "pianist", "guitarist", "drummer", "rapper", "activist", "cowriter", "featuring", "introducing", "starring"]
+	wordThreshold = 3
+	for i in range(wordThreshold):
+		word, offset = getNextWord(offset, text)
+		word = word.lower()
+		if word != "":
+			word = removeSpecialCharacter(word)
+			for ele in array:
+				ele = ele.lower()
+				if ele.startswith(word):
+					return True
+		else:
+			break;
+	return False
+
+# def getPreviousName(offset, text):
+# 	word = ""
+# 	offset = offset - 1;
+# 	name = ""
+# 	flag = True
+# 	count = 0;
+# 	while flag == True:
+# 		while text[offset] != ",":
+# 			name = text[offset] + name
+# 			# print(name)
+# 			offset = offset - 1;
+# 		name = name.replace(" and", "")
+# 		print(name)
+# 		if allWordsCapitalized(name):
+# 			count = count + 1;
+# 			offset = offset - 1
+# 			name = ""
+# 		else:
+# 			flag = False
+# 	if count > 0:
+# 		return True
+# 	else:
+# 		return False
+
+
+
+# def partOfMultipleNames(offset, document):
+# 	text = getDocumentContent(document);
+# 	print(getPreviousName(offset, text))
+
 
 def main():
-	text = getDocumentContent(102);
-	index = text.index("Martin Scorsese's")
-	print(index)
+	text = getDocumentContent(104);
+	index = text.index("Jools Holland")
+	# print(partOfMultipleNames(index, 104))
+	# print(index)
+	# word = "Martin Scorsese's"
+	# word = removeApostrophS(word);
+	# print(removeSpecialCharacter(word))
 	# isPartial(234, 101)
 	# flag = isStartOfSentence(1574, 101)
 	# print(flag)
 	# flag = isContainSuffix("junior jr")
 	# print(flag)
-	flag = hasPartialNameOccurence(794, 102, "Martin Scorsese's")
-	print(flag)
+	# flag = hasPartialNameOccurence(794, 102, "Martin Scorsese's")
+	# print(flag)
 
 main()
