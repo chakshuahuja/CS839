eaf3365c138daf0a9dc8fa52f8a5a18de3a550a3
Chakshu Ahuja <ahuja.chaks@gmail.com>
2019-03-06 17:54:24 -0600

    Add labels for all the candidates

--- tokenizer.py
+++ tokenizer.py
@@ -3,6 +3,9 @@ import re
 
 class Tokenizer:
 
+	start_tag = "<b>"
+	end_tag = "</b>"
+
 	def __init__(self, fname):
 		self.fname = fname
 		self.fidentifier = self.fname.split('/')[-1].strip('.txt')
@@ -13,6 +16,23 @@ class Tokenizer:
 		with open(fname,'r') as f:
 			self.fcontents = f.read()
 			
+	def get_label(self, word):
+		# Returns label 0 (Not an entity) or 1 (Is an entity)
+		entity_word = re.search("(.*)" + Tokenizer.start_tag + "(.+?)" + Tokenizer.end_tag + "(.*)", word)
+		if entity_word:
+			print(entity_word.groups())
+			if re.findall(r"\w+", entity_word.groups()[0]) or re.findall(r"\w+", entity_word.groups()[-1]):
+				return 0
+			return 1
+
+		return 0
+
+	def clean_token(self, token):
+		# Remove the start and end tags
+		return token.replace(Tokenizer.start_tag, '').replace(Tokenizer.end_tag, '')
+
+	def get_offset(self, start_tag_count, end_tag_count):
+		return len(Tokenizer.start_tag) * start_tag_count - len(Tokenizer.end_tag) * end_tag_count
 
 	def tokenize(self, maximum_len=4):
 
@@ -26,34 +46,34 @@ class Tokenizer:
 
 		unitokens = list(enumerate_words(self.fcontents))
 
-		start_tag = "<b>"
-		end_tag = "</b>"
-
-
 		for curr_len in range(1, maximum_len + 1):
 			# Reset the count of start and end tags
 			curr_start_tag = 0
 			curr_end_tag = 0
 
-			for tid in range(0, len(unitokens), curr_len):
+			for tid in range(0, len(unitokens)):
 				# TODO: fix for the last overflow cases
-				print(tid + curr_len)
+				if tid + curr_len > len(unitokens):
+					break
 				curr_token_location_pair = unitokens[tid:tid+curr_len]
-				
-				curr_token = ' '.join([t[0] for t in curr_token_location_pair]).replace(start_tag, '').replace(end_tag, '')
-				curr_location = curr_token_location_pair[0][1] - len(start_tag)*curr_start_tag - len(end_tag)*curr_end_tag
 
-				curr_start_tag += curr_token.count(start_tag)
-				curr_end_tag += curr_token.count(end_tag)
+				curr_unprocessed_token = ' '.join([t[0] for t in curr_token_location_pair])
+				curr_token = self.clean_token(curr_unprocessed_token)
+				curr_location = curr_token_location_pair[0][1] - self.get_offset(curr_start_tag, curr_end_tag)
 
-				self.tokens.append((curr_token, curr_location))
+				curr_label = self.get_label(curr_unprocessed_token)
+
+				curr_start_tag += curr_token.count(Tokenizer.start_tag)
+				curr_end_tag += curr_token.count(Tokenizer.end_tag)
+
+				self.tokens.append((curr_token, curr_location, curr_label))
 
 		return self.tokens
 
 	def print_tokens(self):
-		for t, tp in self.tokens:
-			print("{f_id} {token} {token_position}".format(f_id=self.fidentifier, token=t, token_position=tp))
+		for t, tp, l in self.tokens:
+			print("{f_id} {label} {token} {token_position}".format(f_id=self.fidentifier, token=t, token_position=tp, label=l))
 
-F = Tokenizer("/Users/chakshu/CSMadison839/001.txt")
+F = Tokenizer("/Users/chakshu/CSMadison839/002.txt")
 F.tokenize()
 F.print_tokens()
