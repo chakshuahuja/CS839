b580ddba48e042bfe34b169af7076fcabfe0a7a5
Chakshu Ahuja <ahuja.chaks@gmail.com>
2019-03-31 13:47:49 -0500

    Fixed some bugs

--- Stage2/amazon_scraper.py
+++ Stage2/amazon_scraper.py
@@ -9,54 +9,84 @@ class AmazonScraper:
         self.write_header_to_csv()
 
     def extract_first_page_books(self):
+        curr_count = len(self.books)
+        counter = 0
         for book in self.driver.find_elements_by_xpath("//div[@id='mainResults']/ul/li"):
             header = book.find_element_by_class_name("s-color-twister-title-link")
             title = header.get_attribute("title")
-            link = header.get_attribute("href")
+            link = header.get_attribute("href").split('ref=')[0]
             author = [t.text for t in book.find_elements_by_class_name("a-spacing-none")][2].lstrip("by ")
+
+            # Not extract and add duplicate books belonging to different categories
+            if link in self.books: continue
+
             self.books[link] = {
+                "counter": counter + curr_count,
                 "link": link,
                 "title": title,
                 "author": author
             }
+            counter += 1
     
     def extract_next_page_books(self):
+        curr_count = len(self.books)
+        counter = 0
         for book in self.driver.find_elements_by_class_name("s-result-item"):
             header = book.find_elements_by_class_name("sg-row")[1]
             title = header.find_element_by_class_name("a-color-base").text
-            link = header.find_element_by_class_name("a-link-normal").get_attribute("href")
+            link = header.find_element_by_class_name("a-link-normal").get_attribute("href").split('ref=')[0]
             author = header.find_element_by_class_name("a-color-secondary").text.split("|")[0].lstrip("by ")
+
+            # Not extract and add duplicate books belonging to different categories
+            if link in self.books: continue
+
             self.books[link] = {
+                "counter": counter + curr_count,
                 "link": link,
                 "title": title,
                 "author": author
             }
+            counter += 1
    
     def get_books_data(self, till_page=3):
-        page_number = 1
-        while page_number < till_page:
-            try:
-                page_url = "https://www.amazon.com/s/ref=lp_1_pg_2/146-0611166-7984053?rh=n%3A283155%2Cn%3A%211000%2Cn%3A1&page=" + str(page_number) + "&ie=UTF8&qid=1553406119"
-                self.driver.get(page_url)
-                self.driver.implicitly_wait(5)
-                print('Extracting books from Page ', page_number)
-                if page_number == 1: self.extract_first_page_books()
-                else: self.extract_next_page_books()
-                print(len(self.books), 'books extracted successfully till now.')
-                page_number += 1
-            except:
-                self.driver.quit()
+        categroy_ids = {
+            'History' : '9',
+            'Mystery' : '18',
+            'Literary' : '17'
+        }
+
+        for topic in categroy_ids:
+            categroy_id = categroy_ids[topic]
+            print('Extracting books from Topic ', topic, ' from ', categroy_id)
+            page_number = 1
+            print('First Page URL')
+            print('https://www.amazon.com/s?rh=n%3A283155%2Cn%3A!1000%2Cn%3A' + categroy_id +'&page=' + str(page_number) + '&qid=1554049001')
+            print()
+            while page_number < till_page:
+                try:
+                    page_url = 'https://www.amazon.com/s?rh=n%3A283155%2Cn%3A!1000%2Cn%3A'+ categroy_id + '&page=' + str(page_number) + '&qid='
+                    self.driver.get(page_url)
+                    self.driver.implicitly_wait(5)
+                    print('Extracting books from Page ', page_number)
+                    if page_number == 1: self.extract_first_page_books()
+                    else: self.extract_next_page_books()
+                    print(len(self.books), 'books extracted successfully till now.')
+                    page_number += 1
+                except:
+                    self.driver.quit()
         
     def get_books_additional_data(self):
 
         for book in self.books:
+            print('Extracting from book '+ str(self.books[book]['counter']) + ': ', self.books[book]['title'])
             self.driver.get(book)
             self.driver.implicitly_wait(5)
             details = self.driver.find_element_by_xpath("//table[@id='productDetailsTable']")
 
             for detail in details.text.split('\n'):
                 if ':' in detail:
-                    k, v = detail.split(':')
+                    k, v = detail.split(':', 1)
+                    if k == "Average Customer Review": continue
                     self.books[book][k.strip()] = v.strip()
 
             # Write to CSV with pre-filled for missing fields
@@ -68,7 +98,7 @@ class AmazonScraper:
 
 
     def write_header_to_csv(self):
-        self.field_names = ['title', 'author', 'link', 'Hardcover', 'Paperback', 'Publisher', 'Language', 'ISBN-10', 'ISBN-13', 'Product Dimensions', 'Shipping Weight', 'Average Customer Review', 'Amazon Best Sellers Rank', 'ASIN']
+        self.field_names = ['counter', 'title', 'author', 'link', 'Hardcover', 'Paperback', 'Publisher', 'Language', 'ISBN-10', 'ISBN-13', 'Product Dimensions', 'Shipping Weight', 'Amazon Best Sellers Rank', 'ASIN']
         self.defaults = {k:'N/A' for k in self.field_names}
 
         with open('amazon_books.csv', mode='w') as csv_file:
